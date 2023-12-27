f39742365f6305e4bb5058fd98ffdd53cbd87920
ArandaLidia <arandagomezlidia@gmail.com>
2023-12-27 10:03:31 +0100

    up

--- EjerciciosPDF/src/VariablesYCondicionales/E20.java
+++ EjerciciosPDF/src/VariablesYCondicionales/E20.java
@@ -1,7 +1,37 @@
-package VariablesYCondicionales;
-/*Pedir una hora de la forma hora, minutos y segundos, y mostrar la hora en el segundo siguiente*/
+import java.util.Scanner;
+
 public class E20 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce una hora en el formato: hh:mm:ss");
+        String hora = scanner.next();
 
+        String[] arrayHora = hora.split(":");
+        int horas = Integer.parseInt(arrayHora[0]);
+        int minutos = Integer.parseInt(arrayHora[1]);
+        int segundos = Integer.parseInt(arrayHora[2]);
+
+        if (horas < 0 || horas >= 24 || minutos < 0 || minutos >= 60 || segundos < 0 || segundos >= 60) {
+            System.out.println("Hora incorrecta");
+        } else {
+            segundos++; // Incrementar los segundos en uno
+
+            if (segundos == 60) {
+                segundos = 0;
+                minutos++;
+
+                if (minutos == 60) {
+                    minutos = 0;
+                    horas++;
+
+                    if (horas == 24) {
+                        horas = 0;
+                    }
+                }
+            }
+
+            // Mostrar la hora incrementada
+            System.out.printf("La hora siguiente es: %02d:%02d:%02d%n", horas, minutos, segundos);
+        }
     }
 }
---
+++ EjerciciosPDF/src/VariablesYCondicionales/E21.java
@@ -0,0 +1,5 @@
+package VariablesYCondicionales;
+/*Pedir una nota numérica entera entre 0 y 10, y mostrar dicha nota de la forma: cero, uno, dos, tres...
+*/
+public class E21 {
+}
